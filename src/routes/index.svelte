5ff657782d81185539901190459a29c0efd1710b
José Galisteo <ceritium@gmail.com>
2022-08-01 18:03:23 +0200

    something

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { onMount } from 'svelte';
+  const data = [
+    ["Head 1", "Head2", "Head3"],
+    ["C11", "C12", "C13"],
+    ["C21", "C22", "C23"],
+  ]
+  let tbodyData = data
+  let withHead = false
+
+  $: {
+    if(withHead) {
+      tbodyData = data.slice(1,data.length + 1)
+    } else {
+      tbodyData = data
+    }
+  }
+
+</script>
+<div class="container p-3">
+  <label for="withHead"> With Head: </label>
+  <input name="withHead" type="checkbox" bind:checked={withHead}>
+  <table class="table table-bordered">
+    {#if withHead}
+      <thead>
+        <tr>
+          {#each data[0] as cell}
+            <th> { cell } </th>
+          {/each}
+        </tr>
+      </thead>
+    {/if}
+    <tbody>
+      {#each tbodyData as row}
+        <tr>
+          {#each row as cell}
+            <td> {cell} </td>
+          {/each}
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
