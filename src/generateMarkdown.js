277f586bf6f76080a7f32e2a30945a1c3e85f8b5
José Galisteo <ceritium@gmail.com>
2022-08-11 23:05:03 +0200

    Refactor generate markdown

--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -1,70 +1,66 @@
- const generateMarkdown = (cols, data) => {
-    let code = ""
+const ALIGNS = {
+  left: ":---",
+  center: ":---:",
+  right: "---:",
+  default: "---",
+}
 
-    const add = (text) => { code = code.concat(text) }
-    const addBl = () =>  add("\n")
+const generateMarkdown = (cols, data) => {
+  let code = ""
 
-   const bold = (text) => `**${text}**`
+  const add = (text) => { code = code.concat(text) }
+  const addBl = () =>  add("\n")
+  const bold = (text) => `**${text}**`
 
-   const padded = (text, colIndex) => {
-      const padding = Math.max(widthCols[colIndex], 6) - text.length
-      for (let i = 0; i < padding; i++) {
-        text += " ";
-      }
-     return text
-   }
+  const padded = (text, colIndex) => {
+  const padding = Math.max(widthCols[colIndex], 6) - text.length
+    for (let i = 0; i < padding; i++) {
+      text += " ";
+    }
+    return text
+  }
 
-   // Formate data
-   data = data.map((row, indexRow) => {
-      return row.map((cell, indexCell) => {
-        const col = cols[indexCell]
-        return (cell && col && col.bold) ? bold(cell) : cell
-      })
-   })
+  // Formate data
+  data = data.map((row, indexRow) => {
+     return row.map((cell, indexCell) => {
+       const col = cols[indexCell]
+       return (cell && col && col.bold) ? bold(cell) : cell
+     })
+  })
 
-   const widthCols = data.reduce((obj, row) => {
+  const widthCols = data.reduce((obj, row) => {
     row.forEach((cell, colIndex) => {
       obj[colIndex] ||= 0
       obj[colIndex] = Math.max(obj[colIndex], cell.length)
     })
 
     return obj
-   }, {})
+  }, {})
 
-    data.forEach((row, indexRow) => {
+  data.forEach((row, indexRow) => {
+    add("| ")
+    row.forEach((cell, indexCell) => {
+      add(padded(cell, indexCell))
+      add(" | ")
+    })
+
+    if(indexRow == 0) {
+      addBl()
       add("| ")
+
       row.forEach((cell, indexCell) => {
-        add(padded(cell, indexCell))
+        const col = cols[indexCell]
+
+        const headAlign = ((col && col.align) ? ALIGNS[col.align] : ALIGNS.default) || ALIGNS.default
+        add(padded(headAlign, indexCell))
+
         add(" | ")
       })
+    }
 
-      if(indexRow == 0) {
-        addBl()
-        add("| ")
-
-        row.forEach((cell, indexCell) => {
-          const col = cols[indexCell]
-          if (col) {
-            if(col.align == "left") {
-              add(padded(":---", indexCell))
-            } else if(col.align == "right") {
-              add(padded("---: ", indexCell))
-            } else if(col.align == "center") {
-              add(padded(":---:", indexCell))
-            } else {
-              add(padded("----", indexCell))
-            }
-          } else {
-            add(padded("----", indexCell))
-          }
-
-          add(" | ")
-        })
-      }
-
-      addBl()
-    })
-    return code
-  }
+    addBl()
+  })
+  return code
+ }
 
 export default generateMarkdown
