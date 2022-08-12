ab266deda9f54bf7617b4ef09a15510079bf5f98
José Galisteo <ceritium@gmail.com>
2022-08-12 16:27:30 +0200

    modal

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -196,6 +196,7 @@
 </script>
 
 <div class="container">
+  <input type="button" value="Import" on:click="{addRow}" />
   <input type="button" value="Add row" on:click="{addRow}" />
   <input type="button" value="Add column" on:click="{addColumn}" />
   <input type="button" value="Reset" on:click="{resetTable}" />
@@ -212,6 +213,28 @@
 </code></pre>
     </div>
 </div>
+
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Paste here</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+    </div>
+  </div>
+</div>
+
 <style>
   .markdown-code {
     background: #eee;
